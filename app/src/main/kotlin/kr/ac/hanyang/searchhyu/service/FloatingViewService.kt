e9723c6ab1daca5b3ceede2545a80fdcc04e1c2c
Han Sangwoo <goldhightree@gmail.com>
2017-05-29 18:35:38 +0900

    Add service

---
+++ app/src/main/kotlin/kr/ac/hanyang/searchhyu/service/FloatingViewService.kt
@@ -0,0 +1,11 @@
+package kr.ac.hanyang.searchhyu.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class FloatingViewService : Service() {
+    override fun onBind(intent: Intent?): IBinder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
