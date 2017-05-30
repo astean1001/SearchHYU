70498b5cda29dc58e2c2595ed80f45171b58a598
Han Sangwoo <goldhightree@gmail.com>
2017-05-30 14:49:16 +0900

    Add movable floating button

--- app/src/main/kotlin/kr/ac/hanyang/searchhyu/service/FloatingViewService.kt
+++ app/src/main/kotlin/kr/ac/hanyang/searchhyu/service/FloatingViewService.kt
@@ -7,15 +7,23 @@ import android.content.Intent
 import android.graphics.PixelFormat
 import android.os.IBinder
 import android.support.v7.view.ContextThemeWrapper
-import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.WindowManager
 import kr.ac.hanyang.searchhyu.R
 
-class FloatingViewService : Service() {
+class FloatingViewService : Service(), View.OnTouchListener {
+
     private lateinit var windowManager: WindowManager
     private lateinit var floatingView: View
+    private lateinit var params: WindowManager.LayoutParams
+
+    private var initialX = 0
+    private var initialY = 0
+
+    private var initialTouchX = 0f
+    private var initialTouchY = 0f
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
@@ -28,16 +36,17 @@ class FloatingViewService : Service() {
         val ctx = ContextThemeWrapper(this, R.style.AppTheme)
         floatingView = LayoutInflater.from(ctx).inflate(R.layout.layout_floating_widget, null)
 
-        val params = WindowManager.LayoutParams(
+        floatingView.findViewById(R.id.launchButton).setOnTouchListener(this)
+
+        params = WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.TYPE_PHONE,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
                 PixelFormat.TRANSLUCENT)
 
-        params.gravity = Gravity.TOP or Gravity.LEFT
         params.x = 0
-        params.y = 100
+        params.y = 0
 
         windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
         windowManager.addView(floatingView, params)
@@ -46,5 +55,28 @@ class FloatingViewService : Service() {
     override fun onDestroy() {
         super.onDestroy()
         windowManager.removeView(floatingView)
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                initialX = params.x
+                initialY = params.y
+
+                initialTouchX = event.rawX
+                initialTouchY = event.rawY
+                return true
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                params.x = initialX + (event.rawX - initialTouchX).toInt()
+                params.y = initialY + (event.rawY - initialTouchY).toInt()
+
+                windowManager.updateViewLayout(floatingView, params)
+                return true
+            }
+        }
+
+        return false
     }
 }
